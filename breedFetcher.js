e53dbfd8f93d0f72cd50e4a30dd64fb4ce516029
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-11 14:05:07 +0000

    Callback arguments: for additional outcomes

--- breedFetcher.js
+++ breedFetcher.js
@@ -8,6 +8,17 @@ const fetchBreedDescription = function(breedName, callback) {
         return callback(error, null);
       }
       const data = JSON.parse(body);
+      // The case where we request a breed from command line that results in an
+      // empty array from the website with no value. (So if the breed doesn't
+      // exist)
+      if (data[0] === undefined) {
+        return callback(
+          null,
+          `No results for the command line argument: ${breedName}\nPlease give a valid 'breed' argument..`
+        );
+      }
+      // If indeed the argument is valid and we receive a result return the
+      // callback with the given values.
       return callback(null, data[0].description);
     }
   );
