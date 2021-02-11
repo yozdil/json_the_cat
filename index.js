4de417804506f3188df75ad7fee128e5941d90ea
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-11 14:05:58 +0000

    the index.js from compass

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { fetchBreedDescription } = require("./breedFetcher");
 const breedName = process.argv[2];
 
 fetchBreedDescription(breedName, (error, desc) => {
-
   if (error) {
     console.log("Error fetch details:", error);
   } else {
